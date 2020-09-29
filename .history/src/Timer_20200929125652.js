8bf301500da8b3237d21cc363bb623b83e16a8c6
Charlie Pelton <charliejp0311@gmail.com>
2020-09-29 13:25:24 -0500

    Done.

--- .history/src/Timer_20200929125652.js
+++ .history/src/Timer_20200929125652.js
@@ -6,14 +6,7 @@ class Timer extends Component {
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
 
-  // add your code here\
-  componentDidMount(){
-    this.interval = setInterval(this.clockTick, 1000)
-  }
-
-  componentWillUnmount(){
-    clearInterval(this.interval)
-  }
+  // add your code here
 
   render() {
     const { time, color } = this.state;
